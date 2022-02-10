bd3d2c7b7faa02330b93041be41774cac1ad0c2a
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-02-10 08:34:09 -0400

    Replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Home from "./routes/home";
@@ -10,7 +10,7 @@ import Technology from "./routes/technology";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
@@ -20,7 +20,7 @@ ReactDOM.render(
           <Route path="*" element={<p>There's nothing here!</p>} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
