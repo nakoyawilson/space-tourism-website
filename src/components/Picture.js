344f30bb087cbb2b538e619fd8b6d1caccc1f548
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-02-10 21:03:38 -0400

    Put image class on img tag instead of picture tag

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,9 +1,13 @@
 const Picture = (props) => {
   return (
-    <picture className={props.imageClass}>
+    <picture>
       <source media="(max-width: 1439px)" srcset={props.landscapePicture} />
       <source media="(min-width: 1440px)" srcset={props.portraitPicture} />
-      <img src={props.landscapePicture} alt={props.altText} />
+      <img
+        src={props.landscapePicture}
+        alt={props.altText}
+        className={props.imageClass}
+      />
     </picture>
   );
 };
